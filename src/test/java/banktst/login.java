83675a3594e4b215576130ac24647fd9b8cb892c
katreen nashaat <katreen.nashaat@yahoo.com>
2024-06-27 13:20:19 +0300

    first commit

---
+++ src/test/java/banktst/login.java
@@ -0,0 +1,20 @@
+package banktst;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login {
+    @Given("i am in the login page")
+    public void i_am_in_the_login_page() {
+
+    }
+    @When("i login with credientail")
+    public void i_login_with_credientail() {
+
+    }
+    @Then("i should be taken to s page")
+    public void i_should_be_taken_to_s_page() {
+
+    }
+}
---
+++ src/test/java/resources/s.feature
@@ -0,0 +1,8 @@
+Feature:login functionallty
+ in order to open bank
+  as user
+  i need to login succesffully
+  Scenario:
+    Given i am in the login page
+    When i login with credientail
+    Then i should be taken to s page
